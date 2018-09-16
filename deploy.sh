c03c2e0b4686883ad7015e7af698f6fbf750ed2d
Tomoyuki Nakatsuka <nakatsuka@liquidinc.asia>
2018-09-16 13:40:38 +0900

    デプロイスクリプト

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+echo "STARTING DEPLOY..."
+echo "DEPLOY TO isucon3"
+ssh isucon@118.27.0.147 "source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/go && git pull && make && sudo systemctl restart torb.go"
+echo "isucon3 deploy done"
+
+echo ""
+echo "DEPLOY TO isucon2"
+ssh isucon@118.27.32.125 "source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/go && git pull && make && sudo systemctl restart torb.go && sudo systemctl reload nginx"
+ssh isucon@118.27.32.125 ": > /var/log/nginx/access.log"
+echo "isucon2 deploy done"
